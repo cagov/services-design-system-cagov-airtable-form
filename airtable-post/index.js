4d5bb9f6ac690f6a5daa7665fd1399fa3c367e49
Chach Sikes <chachsikes@ChachSikes.local>
2022-01-10 18:06:58 -0800

    update airtable-form

--- airtable-post/index.js
+++ airtable-post/index.js
@@ -1,36 +1,23 @@
 const CreateAirtableRecord = require("./create-airtable-record.js");
 
-// https://docs.microsoft.com/en-us/azure/azure-functions/functions-reference-node?tabs=v2#node-version
-// myTrigger, myInput, myOtherInput
-const postData = (context, request) => {
-  // let body = "";
-  // request.on("data", async data => {
-  //   body = body + data;
-  console.log(request.body);
-    const airtablePost = await CreateAirtableRecord.init(request.body);
-  //   const jsonSuccess = {
-  //     method: "POST",
-  //     message: "Submitted to airtable",
-  //     data
-  //   };
-  //   console.log("SUCCESS", jsonSuccess, airtablePost);
+module.exports = async function (context, req) {
+  context.log(
+    "JavaScript airtable-form HTTP trigger function processed a request."
+  );
 
-  //   context.res = {
-  //     status: 200,
-  //     "Content-Type": "text/json"
-  //   };
-  // });
+  const body = req.body;
+
+  const airtablePost = CreateAirtableRecord.init(body);
+
+  const responseMessage = {
+    method: "POST",
+    message: "Submitted to airtable",
+    data: airtablePost
+  };
+
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: responseMessage
+  };
 };
 
-module.exports = async function (context, request) {
-  try {
-    // REFS
-    // const appName = context.executionContext.functionName;
-    postData(request, context.response);
-  } catch (e) {
-    context.response = {
-      status: 500,
-      body: `Error - ${e.message}`
-    };
-  }
-};
