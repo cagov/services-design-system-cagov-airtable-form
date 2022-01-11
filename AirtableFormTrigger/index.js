bd2dd284f315af7051d1b1194e8c5810485e0304
Chach Sikes <chachsikes@ChachSikes.local>
2022-01-11 06:38:25 -0800

    Testing server response

--- AirtableFormTrigger/index.js
+++ AirtableFormTrigger/index.js
@@ -2,17 +2,24 @@ const CreateAirtableRecord = require("./create-airtable-record.js");
 
 // Default script (WORKS)
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  context.log("JavaScript HTTP trigger function processed a request.");
 
-    // const name = (req.query.name || (req.body && req.body.name));
-
-    const postRequest = CreateAirtableRecord.init(req.body);
-
-    const responseMessage = "Success";
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+  // const name = (req.query.name || (req.body && req.body.name));
+  try {
+    const data = {
+      fields: req.body.fields,
+      options: req.body.options
     };
-}
+  } catch (error) {
+    context.log(error);
+  }
+  // const postRequest = CreateAirtableRecord.init(body);
 
+  const responseMessage = "Success";
+
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: responseMessage,
+    error: error
+  };
+};
--- README.md
+++ README.md
@@ -5,4 +5,7 @@ curl  -d "Name=CurlNamePOSTRequest&Email=test@example.com&Request=Request1"  -X 
 
 curl  -d "Name=CurlNamePOSTRequest&Email=test@example.com&Request=Request1"  -X POST http://localhost:7071/airtable-post
 
-az functionapp config set --linux-fx-version "node|14" 
+curl -X POST https://fa-go-design-system-cagov-airtable-form.azurewebsites.net/api/AirtableFormTrigger?code=Zu6Ck/103KrMh2mp/7CANtN3dMrtHMpjdhayZm82OnGava5/A1urvQ==
+   -H "Content-Type: application/json"
+   -d '{"fields": {"Name": "chach", "Email": "chach.sikes@digital.ca.gov", "Request": "Request1"}, "options": {"database": "app4B5KqaCTSHAdCS", "table": "01 - Design System Launch"
+}}'  
