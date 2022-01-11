39d9b2dc26630bcfcf869606fb19f8266da7da82
Chach Sikes <chachsikes@ChachSikes.local>
2022-01-10 19:38:24 -0800

    debugging

--- AirtableFormTrigger/index.js
+++ AirtableFormTrigger/index.js
@@ -1,41 +1,17 @@
 const CreateAirtableRecord = require("./create-airtable-record.js");
 
-module.exports = async function (context, req) {
-  context.log(
-    "JavaScript airtable-form HTTP trigger function processed a request."
-  );
-
-  const body = req.body;
-
-  context.log(body);
-  const airtablePost = CreateAirtableRecord.init(body);
-
-  const responseMessage = {
-    method: "POST",
-    message: "Submitted to airtable",
-    data: airtablePost,
-    body: body
-  };
-
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: responseMessage
-  };
-};
-
-
 // Default script (WORKS)
-// module.exports = async function (context, req) {
-//     context.log('JavaScript HTTP trigger function processed a request.');
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
 
-//     const name = (req.query.name || (req.body && req.body.name));
-//     const responseMessage = name
-//         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-//         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-//     context.res = {
-//         // status: 200, /* Defaults to 200 */
-//         body: responseMessage
-//     };
-// }
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
 
